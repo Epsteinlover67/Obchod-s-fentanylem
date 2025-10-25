bc6de156e554b330bd564fc38e95c9b6e2219ee8
Radim Bobek <radimb2707@gmail.com>
2025-10-25 16:48:18 +0200

    Bomba

--- Script.js
+++ Script.js
@@ -8,3 +8,27 @@ function showPage(pageId) {
     activePage.classList.add('active');
   }
 }
+
+const furrytheme = document.getElementById('furrytheme');
+let currentTheme = 0;
+
+furrytheme.addEventListener('click', () => {
+  currentTheme = (currentTheme + 1) % 2;
+
+  switch (currentTheme) {
+    case 0:
+      document.documentElement.style.setProperty('--bg-color', '#1e1e2f');
+      document.documentElement.style.setProperty('--text-color', '#EAEAEA');
+      document.documentElement.style.setProperty('--accent-color', '#7c5cff');
+      document.documentElement.style.setProperty('--nav-color', '#2c2c3e');
+      break;
+    case 1: 
+      document.documentElement.style.setProperty("--bg-color", "#1A1025");
+      document.documentElement.style.setProperty("--text-color", "#FF80E0");
+      document.documentElement.style.setProperty("--accent-color", "#FF5AF1");
+      document.documentElement.style.setProperty("--nav-color", "#27143A");
+      break;
+  }
+}); 
+      
+      
